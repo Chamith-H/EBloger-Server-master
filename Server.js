0a2def8252d9ca04431356830a68130fa89d9e3b
Chamith-H <chamith.softwaredev@gmail.com>
2023-05-20 22:39:50 +0530

    dd

--- Server.js
+++ Server.js
@@ -16,6 +16,6 @@ mongoose.connect(clusterURL)
 app.use("/", require("./Routes/Users_Route"));
 
 //Testing Back-End
-app.listen(8000, 'https://e-bloger-server-master.vercel.app/', function() {
+app.listen('https://e-bloger-server-master.vercel.app/', function() {
     console.log("Server was up")
 })
